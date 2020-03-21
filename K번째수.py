b2a6e7416d47a0d85c018426676a74f57fe6764d
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 02:54:14 +0900

    Update K번째수.py

--- K번째수.py
+++ K번째수.py
@@ -2,7 +2,7 @@ def solution(array, commands):
     answer = []
     
     for command in commands:
-        new_array = array[command[0]-1:command[1]]
-        new_array.sort()
-        answer.append(new_array[command[2]-1])
+        new_array = array[command[0]-1:command[1]] # i 부터 j 범위까지 숫자
+        new_array.sort() #  정렬
+        answer.append(new_array[command[2]-1]) # #k번째 숫자
     return answer
