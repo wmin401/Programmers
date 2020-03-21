55105290295ea730da46a89f1235389adf9829cc
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 05:33:36 +0900

    Update 문자열 내 마음대로 정렬하기.py

--- 문자열 내 마음대로 정렬하기.py
+++ 문자열 내 마음대로 정렬하기.py
@@ -1,3 +1,3 @@
 def solution(strings, n):
-    answer = sorted(sorted(strings), key = lambda x: x[n])
-    return answer
+    answer = sorted(sorted(strings), key = lambda x: x[n]) # strings 배열을 오름차 순으로 먼저 정리하고, 
+    return answer # 또 한 번 정렬하는데 lambda 함수를 이용해 n 자리수 기준으로 오름차 정렬 하게끔 한다.
