1169c9d1f3b094727481d1f176291bd81f84fa91
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 05:43:42 +0900

    Update 완주하지 못한 선수.py

--- 완주하지 못한 선수.py
+++ 완주하지 못한 선수.py
@@ -1,9 +1,9 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
+    participant.sort() # 참가자 오름차순 정렬
+    completion.sort() # 완주자 오름차순 정렬
     
-    for i,j in zip(participant,completion):
-        if i != j :
+    for i,j in zip(participant,completion): # zip 함수를 사용하여 하나의 묶으로 바꾸고 
+        if i != j : # i 와 j 를 비교한다.
             return i
     
-    return participant[-1]
+    return participant[-1] # i와 j를 비교해서 일치 하지 않을 경우 참가자 마지막 사람을 리턴한다.
