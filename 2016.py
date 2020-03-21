feec991e9877b7a49be48deccb4ec1f8cb7c27c2
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 00:37:06 +0900

    Update 2016.py

--- 2016.py
+++ 2016.py
@@ -1,6 +1,6 @@
-import datetime
+import datetime # datetime 라이브러리 호출
 
 def solution(a, b):
-    day_of_the_week = ['MON','TUE','WED','THU','FRI','SAT','SUN']
-    answer = day_of_the_week[datetime.datetime(2016,a,b).weekday()]
+    day_of_the_week = ['MON','TUE','WED','THU','FRI','SAT','SUN']  # 날짜 리스트
+    answer = day_of_the_week[datetime.datetime(2016,a,b).weekday()] # weakday() 함수 사용 (화요일은 1을 반환함)
     return answer
