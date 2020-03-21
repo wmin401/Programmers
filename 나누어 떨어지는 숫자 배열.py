ca60ebbf470fd06bad7914be9b3465ec4d110897
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 05:27:13 +0900

    Update 나누어 떨어지는 숫자 배열.py

--- 나누어 떨어지는 숫자 배열.py
+++ 나누어 떨어지는 숫자 배열.py
@@ -1,11 +1,11 @@
 def solution(arr, divisor):
     answer = []
     for i in arr:
-        if i%divisor == 0:
-            answer.append(i)
+        if i%divisor == 0: # divior로 나눠지는 확인
+            answer.append(i) # 나눠지면 anwer 배열에 추가한다.
         
-    if len(answer) == 0:
-        answer.append(-1)
+    if len(answer) == 0: # 나눠진 숫자가 없을 경우
+        answer.append(-1) # -1 을 추가한다.
         
     answer.sort()
     return answer
