0a4546356eb16f58f636da3b83295813ed758098
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 05:29:58 +0900

    Update 두 정수 사이의 합.py

--- 두 정수 사이의 합.py
+++ 두 정수 사이의 합.py
@@ -1,7 +1,7 @@
 def solution(a, b):
     answer = 0
     
-    for i in range(min(a,b),max(a,b)+1):
-        answer += i
+    for i in range(min(a,b),max(a,b)+1): # min(a,b) a, b 중 작은 수, max(a,b) a,b 중 큰 수 로 범위 설정
+        answer += i # 범위 내의 숫자를 모두 더한다.
         
     return answer
