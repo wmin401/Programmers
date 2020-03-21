7b98c843378ea278081a06da99e9f4a3efabbc14
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 05:30:42 +0900

    Update 문자열 내 p와 y의 개수.py

--- 문자열 내 p와 y의 개수.py
+++ 문자열 내 p와 y의 개수.py
@@ -1,4 +1,4 @@
 def solution(s):
-    answer = s.lower().count('p') == s.lower().count('y')
+    answer = s.lower().count('p') == s.lower().count('y') # 모두 소문자로 변환하고 갯수를 비교하는 논리식
 
     return answer
