80c2c7fdbd67c1cd4b82865542ac77fa6d6c70b5
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 05:47:43 +0900

    Update 모의고사.py

--- 모의고사.py
+++ 모의고사.py
@@ -6,15 +6,15 @@ def solution(answers):
     pattern3 = [3,3,1,1,2,2,4,4,5,5]
 
     for i in range(len(answers)):
-        if answers[i] == pattern1[i%len(pattern1)]:
-            scores[0] += 1
-        if answers[i] == pattern2[i%len(pattern2)]:
+        if answers[i] == pattern1[i%len(pattern1)]: # pattern1 정답과 비교
+            scores[0] += 1 # 정답과 일치하면 추가한다.
+        if answers[i] == pattern2[i%len(pattern2)]: # pattern2 정답과 비교
             scores[1] += 1
-        if answers[i] == pattern3[i%len(pattern3)]:
+        if answers[i] == pattern3[i%len(pattern3)]: # pattern3 정답과 비교
             scores[2] += 1 
 
-    max_score = max(scores)
+    max_score = max(scores) # 점수중 최고값 산출
     for i in range(len(scores)):
-        if scores[i] == max_score:
-            answer.append(i+1)
+        if scores[i] == max_score: # 각 패턴과 최고값 비교
+            answer.append(i+1) # 최고값 위치 
     return answer
