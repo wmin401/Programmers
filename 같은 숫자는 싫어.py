d4fc2ba5daab69fbfa4f4b3a223c4d9f278c9096
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 05:25:48 +0900

    Update 같은 숫자는 싫어.py

--- 같은 숫자는 싫어.py
+++ 같은 숫자는 싫어.py
@@ -2,8 +2,8 @@ def solution(arr):
     answer = []
     
     for i in arr:
-        if [i] == answer[-1:]:
-            continue
-        answer.append(i)
+        if [i] == answer[-1:]: # 가장 최근 더해진 숫자와 비교
+            continue # 위의 논리식이 True 이면 다시 위로
+        answer.append(i) # 가장 최근 더해진 숫자와 비교가 False 라면 answer 배열에 추가한다.
     
     return answer
