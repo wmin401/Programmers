072242b43658b6de957aabb1f0bb73e1d993f678
wmin401 <52664613+wmin401@users.noreply.github.com>
2020-03-22 05:35:28 +0900

    Update 문자열 다루기 기본.py

--- 문자열 다루기 기본.py
+++ 문자열 다루기 기본.py
@@ -1,5 +1,5 @@
 def solution(s):
-    if (len(s) == 4 or len(s) == 6) and s.isdigit() == True:
+    if (len(s) == 4 or len(s) == 6) and s.isdigit() == True: # 문자열 길이가 4 or 6 인지 비교하고, isdigit() 함수를 사용하여 숫자로만 되어있는지 확인한다.
         answer = True
     else:
         answer = False
